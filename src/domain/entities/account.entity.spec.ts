73902f129ea04e32cda221f8e7f1adc048abc55e
FIRMINOenzo <enzofirmino.camp@gmail.com>
2025-10-28 20:26:09 -0300

    feat: enhance AccountEntity with permission-based updates and role management

--- src/domain/entities/account.entity.spec.ts
+++ src/domain/entities/account.entity.spec.ts
@@ -1,13 +1,47 @@
+import { ForbiddenError } from '../errors/forbidden.error';
 import { ValueObjectValidationError } from '../errors/value-object-validation.error';
+import {
+  Permission,
+  PermissionAction,
+  PermissionSubject,
+} from '../value-objects';
 import { AccountEntity } from './account.entity';
+import { RoleEntity, RoleName } from './role.entity';
 
 describe('Account Entity', () => {
+  const updateAccountPermission = new Permission(
+    PermissionAction.UPDATE,
+    PermissionSubject.ACCOUNT,
+  );
+  const accountRole = new RoleEntity(crypto.randomUUID(), RoleName.ADMIN, [
+    updateAccountPermission,
+  ]);
+  const accountWithPermission = new AccountEntity(
+    crypto.randomUUID(),
+    'John Doe',
+    'john.doe@example.com',
+    'Password123',
+    new Date(),
+    new Date(),
+    accountRole,
+  );
+  const accountWithoutPermission = new AccountEntity(
+    crypto.randomUUID(),
+    'John Doe',
+    'john.doe@example.com',
+    'Password123',
+    new Date(),
+    new Date(),
+    new RoleEntity(crypto.randomUUID(), RoleName.READER),
+  );
+
   describe('Create', () => {
     test('should create a valid account', () => {
       const account = AccountEntity.create(
         'John Doe',
         'john.doe@example.com',
         'Password123',
+        new RoleEntity(crypto.randomUUID(), RoleName.ADMIN),
       );
       expect(account.getId()).toBeDefined();
       expect(account.getName()).toBe('John Doe');
@@ -17,7 +51,12 @@ describe('Account Entity', () => {
 
     test('should throw an error when creating an account with an invalid information', () => {
       expect(() =>
-        AccountEntity.create('', 'john.doe@example.com', 'Password123'),
+        AccountEntity.create(
+          '',
+          'john.doe@example.com',
+          'Password123',
+          new RoleEntity(crypto.randomUUID(), RoleName.ADMIN),
+        ),
       ).toThrow(
         new ValueObjectValidationError(
           'Name must be at least 1 character long',
@@ -32,15 +71,15 @@ describe('Account Entity', () => {
         'John Doe',
         'john.doe@example.com',
         'Password123',
+        new RoleEntity(crypto.randomUUID(), RoleName.ADMIN),
       );
       account.updateInformation(
+        accountWithPermission,
         'John Doe 2°',
         'john.doe2@example.com',
-        'Password234@',
       );
       expect(account.getName()).toBe('John Doe 2°');
       expect(account.getEmail()).toBe('john.doe2@example.com');
-      expect(account.getPassword()).toBe('Password234@');
     });
 
     test('should throw an error when updating the account information with an invalid information', () => {
@@ -48,10 +87,112 @@ describe('Account Entity', () => {
         'John Doe',
         'john.doe@example.com',
         'Password123',
+        accountRole,
       );
       expect(() =>
-        account.updateInformation('John Doe 2°', 'john.doe2', 'Password234@'),
+        account.updateInformation(
+          accountWithPermission,
+          'John Doe 2°',
+          'john.doe2',
+        ),
       ).toThrow(new ValueObjectValidationError('Invalid email address'));
+    });
+
+    test('should throw an error when updating the account information without permission', () => {
+      const account = AccountEntity.create(
+        'John Doe',
+        'john.doe@example.com',
+        'Password123',
+        new RoleEntity(crypto.randomUUID(), RoleName.ADMIN),
+      );
+      expect(() =>
+        account.updateInformation(
+          accountWithoutPermission,
+          'John Doe 2°',
+          'john.doe2@example.com',
+        ),
+      ).toThrow(
+        new ForbiddenError(
+          'You are not allowed to update the account information',
+        ),
+      );
+    });
+  });
+
+  describe('Change Password', () => {
+    test('should change the account password', () => {
+      const account = AccountEntity.create(
+        'John Doe',
+        'john.doe@example.com',
+        'Password123',
+        accountRole,
+      );
+      account.changePassword(account, 'Password123@');
+      expect(account.getPassword()).toBe('Password123@');
+    });
+
+    test('should throw an error when changing the account password without permission', () => {
+      const account = AccountEntity.create(
+        'John Doe',
+        'john.doe@example.com',
+        'Password123',
+        accountRole,
+      );
+      expect(() =>
+        account.changePassword(accountWithoutPermission, 'Password123@'),
+      ).toThrow(
+        new ForbiddenError(
+          'You are not allowed to update the account password',
+        ),
+      );
+    });
+
+    test('should throw an error when changing the account password with an invalid password', () => {
+      const account = AccountEntity.create(
+        'John Doe',
+        'john.doe@example.com',
+        'Password123',
+        accountRole,
+      );
+      expect(() => account.changePassword(account, 'invalid')).toThrow(
+        new ValueObjectValidationError(
+          'Password must be at least 8 characters long',
+        ),
+      );
+    });
+  });
+
+  describe('Set Role', () => {
+    test('should set the account role', () => {
+      const account = AccountEntity.create(
+        'John Doe',
+        'john.doe@example.com',
+        'Password123',
+        accountRole,
+      );
+      account.setRole(
+        accountWithPermission,
+        new RoleEntity(crypto.randomUUID(), RoleName.ADMIN),
+      );
+      expect(account.getRole()).toBeDefined();
+      expect(account.getRole()?.getName()).toBe('ADMIN');
+    });
+
+    test('should throw an error when setting the account role without permission', () => {
+      const account = AccountEntity.create(
+        'John Doe',
+        'john.doe@example.com',
+        'Password123',
+        accountRole,
+      );
+      expect(() =>
+        account.setRole(
+          accountWithoutPermission,
+          new RoleEntity(crypto.randomUUID(), RoleName.ADMIN),
+        ),
+      ).toThrow(
+        new ForbiddenError('You are not allowed to update the account role'),
+      );
     });
   });
 });
--- src/domain/entities/account.entity.ts
+++ src/domain/entities/account.entity.ts
@@ -1,27 +1,97 @@
-import { Email, Name, Password, UUID } from '../value-objects';
+import { ForbiddenError } from '../errors/forbidden.error';
+import {
+  Email,
+  Name,
+  Password,
+  UUID,
+  PermissionAction,
+  PermissionSubject,
+} from '../value-objects';
+import { RoleEntity } from './role.entity';
 
 export class AccountEntity {
   private readonly id: UUID;
+  private readonly createdAt: Date;
+  private updatedAt: Date;
   private name: Name;
   private email: Email;
   private password: Password;
+  private role: RoleEntity | null;
 
-  constructor(id: string, name: string, email: string, password: string) {
+  constructor(
+    id: string,
+    name: string,
+    email: string,
+    password: string,
+    createdAt: Date,
+    updatedAt: Date,
+    role?: RoleEntity,
+  ) {
     this.id = new UUID(id);
+    this.createdAt = createdAt;
+    this.updatedAt = updatedAt;
     this.name = new Name(name);
     this.email = new Email(email);
     this.password = new Password(password);
+    this.role = role ?? null;
   }
 
-  static create(name: string, email: string, password: string): AccountEntity {
+  static create(
+    name: string,
+    email: string,
+    password: string,
+    role: RoleEntity,
+  ): AccountEntity {
     const id = crypto.randomUUID();
-    return new AccountEntity(id, name, email, password);
+    return new AccountEntity(
+      id,
+      name,
+      email,
+      password,
+      new Date(),
+      new Date(),
+      role,
+    );
   }
 
-  updateInformation(name: string, email: string, password: string): void {
-    this.name = new Name(name);
-    this.email = new Email(email);
-    this.password = new Password(password);
+  updateInformation(
+    allowedBy: AccountEntity,
+    name?: string,
+    email?: string,
+  ): void {
+    if (
+      !allowedBy
+        .getRole()
+        ?.hasPermission(PermissionAction.UPDATE, PermissionSubject.ACCOUNT)
+    ) {
+      throw new ForbiddenError(
+        'You are not allowed to update the account information',
+      );
+    }
+
+    let changed = false;
+
+    if (name) {
+      this.name = new Name(name);
+      changed = true;
+    }
+    if (email) {
+      this.email = new Email(email);
+      changed = true;
+    }
+
+    if (changed) this.updatedAt = new Date();
+  }
+
+  changePassword(allowedBy: AccountEntity, newPassword: string): void {
+    if (!allowedBy.equals(this)) {
+      throw new ForbiddenError(
+        'You are not allowed to update the account password',
+      );
+    }
+
+    this.password = new Password(newPassword);
+    this.updatedAt = new Date();
   }
 
   getId(): string {
@@ -38,5 +108,36 @@ export class AccountEntity {
 
   getPassword(): string {
     return this.password.getValue();
+  }
+
+  getRole(): RoleEntity | null {
+    return this.role;
+  }
+
+  setRole(allowedBy: AccountEntity, role: RoleEntity): void {
+    if (
+      !allowedBy
+        .getRole()
+        ?.hasPermission(PermissionAction.UPDATE, PermissionSubject.ACCOUNT)
+    ) {
+      throw new ForbiddenError(
+        'You are not allowed to update the account role',
+      );
+    }
+
+    this.role = role;
+    this.updatedAt = new Date();
+  }
+
+  private equals(account: AccountEntity): boolean {
+    return this.getId() === account.getId();
+  }
+
+  getCreatedAt(): Date {
+    return this.createdAt;
+  }
+
+  getUpdatedAt(): Date {
+    return this.updatedAt;
   }
 }
