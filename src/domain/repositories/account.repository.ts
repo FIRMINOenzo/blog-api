155aac305213bbc9cbe52364c764acf30e124349
FIRMINOenzo <enzofirmino.camp@gmail.com>
2025-10-27 21:19:39 -0300

    feat: add domain AccountRepository interface

---
+++ src/domain/repositories/account.repository.ts
@@ -0,0 +1,8 @@
+import { AccountEntity } from '../entities/account.entity';
+
+export interface AccountRepository {
+  create(account: AccountEntity): Promise<void>;
+  findById(id: string): Promise<AccountEntity | null>;
+  findByEmail(email: string): Promise<AccountEntity | null>;
+  update(account: AccountEntity): Promise<void>;
+}
