89d7b113a8ce809836be62bd9e900ba0965c4767
FIRMINOenzo <enzofirmino.camp@gmail.com>
2025-10-28 20:26:28 -0300

    feat: create RoleRepository interface for role management

---
+++ src/domain/repositories/role.repository.ts
@@ -0,0 +1,6 @@
+import { RoleEntity } from '../entities/role.entity';
+
+export interface RoleRepository {
+  findById(id: string): Promise<RoleEntity | null>;
+  findByName(name: string): Promise<RoleEntity | null>;
+}
