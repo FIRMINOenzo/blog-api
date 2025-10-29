75754e2ec9e591907695c3bba5c5293a62f6c13a
FIRMINOenzo <enzofirmino.camp@gmail.com>
2025-10-29 15:25:23 -0300

    feat: integrate AuthModule and secure account creation with JWT guard

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AccountModule } from './modules/account/account.module';
+import { AuthModule } from './modules/auth/auth.module';
 import { DatabaseModule } from './infra/database/database.module';
 
 @Module({
-  imports: [DatabaseModule, AccountModule],
+  imports: [DatabaseModule, AuthModule, AccountModule],
 })
 export class AppModule {}
--- src/modules/account/account.controller.ts
+++ src/modules/account/account.controller.ts
@@ -1,35 +1,20 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { CreateAccountUseCase } from './usecases/create-account.usecase';
 import { CreateAccountDto } from './dto/create-account.dto';
+import { CurrentUser } from '../auth/decorators/current-user.decorator';
 import { AccountEntity } from 'src/domain/entities/account.entity';
-import { RoleEntity } from 'src/domain/entities/role.entity';
-import {
-  Permission,
-  PermissionAction,
-  PermissionSubject,
-} from 'src/domain/value-objects';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 
 @Controller('accounts')
+@UseGuards(JwtAuthGuard)
 export class AccountController {
   constructor(private readonly createAccountUseCase: CreateAccountUseCase) {}
 
   @Post()
-  async create(@Body() input: CreateAccountDto) {
-    const mockAdminAccount = new AccountEntity(
-      crypto.randomUUID(),
-      'Admin',
-      'admin@example.com',
-      'hashed_password',
-      new Date(),
-      new Date(),
-      new RoleEntity(crypto.randomUUID(), 'ADMIN', [
-        new Permission(PermissionAction.CREATE, PermissionSubject.ACCOUNT),
-        new Permission(PermissionAction.READ, PermissionSubject.ACCOUNT),
-        new Permission(PermissionAction.UPDATE, PermissionSubject.ACCOUNT),
-        new Permission(PermissionAction.DELETE, PermissionSubject.ACCOUNT),
-      ]),
-    );
-
-    return this.createAccountUseCase.execute(mockAdminAccount, input);
+  async create(
+    @Body() input: CreateAccountDto,
+    @CurrentUser() currentUser: AccountEntity,
+  ) {
+    return this.createAccountUseCase.execute(currentUser, input);
   }
 }
