b2d4094f619ca6ef8721abf42253909b56490adb
FIRMINOenzo <enzofirmino.camp@gmail.com>
2025-10-29 15:25:46 -0300

    feat: add global exception filter to main application bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { DomainExceptionFilter } from './infra/filters/domain-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.useGlobalFilters(new DomainExceptionFilter());
   await app.listen(process.env.PORT ?? 3000, () =>
     console.log(`App running on port ${process.env.PORT ?? 3000}`),
   );
