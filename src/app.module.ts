ab0f546199cdb9ff6eb25075e4a968abec009dc3
FIRMINOenzo <enzofirmino.camp@gmail.com>
2025-10-30 16:25:18 -0300

    feat: add ArticleModule to AppModule imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AccountModule } from './modules/account/account.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { ArticleModule } from './modules/article/article.module';
 import { DatabaseModule } from './infra/database/database.module';
 
 @Module({
-  imports: [DatabaseModule, AuthModule, AccountModule],
+  imports: [DatabaseModule, AuthModule, AccountModule, ArticleModule],
 })
 export class AppModule {}
